334b7e00fb0998c02174bb31b505c2eea05505ea
NOOB_3301 <sepic466@gmail.com>
2024-08-21 22:35:31 +0530

    ...............

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ app.get("/apiv2/manga/image/:coverid/:filename", async (req, res) => {
     res.send(`This page is about coverid and filename: ${coverid}, ${filename}`);
 });
 
-app.get("/manga/info/:query", async (req, res) => {
+app.get("/aviv2/manga/info/:query", async (req, res) => {
     let s_query = req.params.query;
     try {
         let response = await fetch_info(manga_url, s_query);
